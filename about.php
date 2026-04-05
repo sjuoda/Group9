a329e5c505327df6dba8c5313dc21f65d81b2df1
Sarunas Juodagalvis <sarunasj@ankara.cs.colostate.edu>
2026-04-05 15:13:21 -0600

    changes to home and about page, about page has structure for team members to add to, css with colors and styling

--- about.php
+++ about.php
@@ -25,6 +25,44 @@
     
     <main>
         
+        <div id="team">
+            <div id="t1">
+                <img src="sarunas1.jpg" alt="image of Sarunas Juodagalvis">
+                <h2>Sarunas Juodagalvis</h2>
+                <p>My name is Sarunas Juodagalvis and I am a senior Computer Science major with a concentration in Computing for
+                    Creatives at Colorado State University. I have a background in software engineering and game design from my courses at CSU, and I'm currently looking for a software development internship.
+                    When I'm not busy with school, you'll probably find me watching F1, sim racing, or cheering on the Colorado Avalanche. I also share my home with 
+                    two Manx cats who think they run the place. Their names are Clover and Hestia.
+                </p>
+            </div>
+
+            <div id="t2">
+                <img src="sarunas1.jpg" alt="image of Sarunas Juodagalvis">
+                <h2>Sarunas Juodagalvis</h2>
+                <p>My name is Sarunas Juodagalvis and I am a senior Computer Science major with a concentration in Computing for
+                    Creatives.
+                </p>
+            </div>
+
+            <div id="t3">
+                <img src="sarunas1.jpg" alt="image of Sarunas Juodagalvis">
+                <h2>Sarunas Juodagalvis</h2>
+                <p>My name is Sarunas Juodagalvis and I am a senior Computer Science major with a concentration in Computing for
+                    Creatives. 
+                </p>
+            </div>
+
+            <div id="t4">
+                <img src="sarunas1.jpg" alt="image of Sarunas Juodagalvis">
+                <h2>Sarunas Juodagalvis</h2>
+                <p>My name is Sarunas Juodagalvis and I am a senior Computer Science major with a concentration in Computing for
+                    Creatives. Welcome to my website.
+                </p>
+            </div>
+
+        </div>
+        
+
     </main>
     <footer>
         <p>&#169 Group 9 Website - CS312 - CSU</p>
--- index.php
+++ index.php
@@ -11,9 +11,9 @@
     <link rel="stylesheet" type="text/css" href="style.css" >
 </head>
 
-<body>
+<body #id="indexBody">
     <header>
-        <h1>Welcome to Group 9</h1>
+        <h1>CLOUD 9 COLORS</h1>
     </header>
     <nav>
         <ul>
@@ -22,8 +22,20 @@
             <li><a href="color.php">Color Coordinator</a></li>
         </ul>
     </nav>
+
+    <img id="logo" src="colored-logo.png" alt="Company logo" width="200">
     
-    <main>
+    <main id="indexMain">
+        <h1> Welcome</h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed porttitor risus, aliquam finibus mi. 
+            Nulla sagittis feugiat scelerisque. Aenean ut ante arcu. Phasellus eleifend, massa nec mattis ultrices, dolor 
+            orci ullamcorper ligula, eget ornare diam justo eget ante. Sed tristique nibh et justo consectetur sodales. 
+            Aenean rhoncus enim at orci porta, et consequat metus pharetra. Nam iaculis, odio non varius auctor, eros diam 
+            consequat ipsum, eget varius diam tellus in odio. Nunc porttitor elit at justo euismod, vel auctor elit vulputate. 
+            Aliquam congue, nisl sit amet efficitur fermentum, risus orci hendrerit massa, ut auctor nisi felis vitae massa.
+        </p>
         
     </main>
     <footer>
